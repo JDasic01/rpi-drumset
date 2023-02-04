456151a2662ebf93f38241a382a0213df5e99cb9
JDasic01 <jdasic1@gmail.com>
2023-02-04 14:24:53 +0100

    preset logic update

--- drum_demo.py
+++ drum_demo.py
@@ -1,5 +1,5 @@
 import pgzrun
-
+import os
 screen_title = "Drum Module Demo"
 # Set Pygame Zero screen size and title
 WIDTH = 800
@@ -7,6 +7,14 @@ HEIGHT = 600
 TITLE = screen_title
 # class Preset:
 
+class Preset:
+    def create_preset_folder(self, preset_name, preset_sounds):
+        '''Take the name of the folder and an array with presets, create the folder as the subfolder of "sounds"'''
+        pass
+     def choose_preset(name):
+        '''Find path to the preset with selected name'''
+        # path = ...
+        # return path
 
 def draw():
     screen.fill((192, 192, 192))
@@ -14,6 +22,7 @@ def draw():
     color=(255, 255, 255), scolor="#202020")
 
 def on_key_down(key):
+    # play(path_from_choose_preset)
     if keyboard.up:
         sounds.hi_hat.set_volume(0.03)
         sounds.hi_hat.play()
