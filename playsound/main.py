93f5358ecd234451f0dfef0fd7f425954b3e867a
JDasic01 <jdasic1@gmail.com>
2023-02-13 17:37:31 +0100

    playsound demo

--- playsound/main.py
+++ playsound/main.py
@@ -1,4 +1,9 @@
 import playsound
+from preset import *
 
-playsound.playsound("OpenHH.wav")
+presets = create_preset_list()
+
+playsound.playsound(presets[0] +"/" "bass.wav")
+
+playsound.playsound(presets[1] +"/" "bass.wav")
 
